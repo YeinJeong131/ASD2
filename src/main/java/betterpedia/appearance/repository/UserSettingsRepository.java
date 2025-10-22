bde7029823682cead7f899dfecdfdbe10a7f7861
Yein Jeong <bella060131@gmail.com>
2025-10-22 21:26:45 +1100

    fix

--- src/main/java/betterpedia/appearance/repository/UserSettingsRepository.java
+++ src/main/java/betterpedia/appearance/repository/UserSettingsRepository.java
@@ -5,13 +5,21 @@ import betterpedia.appearance.entity.UserSettings;
 
 import java.util.Optional;
 import betterpedia.user.entity.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserSettingsRepository extends JpaRepository<UserSettings, Long> {
-    Optional<UserSettings> findByUserId(Long userId); // restrict checking null
-    boolean existsByUserId(Long userId);
+//    Optional<UserSettings> findByUserId(Long userId); // restrict checking null
+//    boolean existsByUserId(Long userId);
 
     Optional<UserSettings> findByUser(User user);
-    boolean existsByUser(User user);
+//    boolean existsByUser(User user);
+
+    @Query("SELECT us FROM UserSettings us WHERE us.user.id = :userId")
+    Optional<UserSettings> findByUserId(@Param("userId") Long userId);
+
+    @Query("SELECT COUNT(us) > 0 FROM UserSettings us WHERE us.user.id = :userId")
+    boolean existsByUserId(@Param("userId") Long userId);
 }
 
 
